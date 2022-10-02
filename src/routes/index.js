f96ff08a454a6824d89ec9a50b7d90f0afc9c745
Fabricio <fabriciospfullstack@gmail.com>
2022-10-02 11:39:35 -0300

    :sparkles: added support for more routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const { Router } = require('express')
 const router = Router()
 
 const appRoutes = require('./app.routes')
-router.use(appRoutes)
+const newsRoutes = require('./news.routes')
+
+router.use('/', appRoutes)
+router.use('/', newsRoutes)
 
 
 module.exports = router
