24137ee6cbbd4414b6c9b975065a28980cbb7e44
Fabricio <fabriciospfullstack@gmail.com>
2022-10-02 14:37:09 -0300

    :sparkles: configure swagger in api-docs route

--- server.js
+++ server.js
@@ -5,5 +5,10 @@ const app = express()
 const routes = require('./src/routes/index')
 app.use(routes)
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const port = 3000 || 5000
 app.listen(port, () => console.log('Server running: http://localhost:'+port))
